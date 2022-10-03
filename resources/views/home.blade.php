468d04fdbeab6cdf7975d061b3b6b3691a12668a
Uvssss <uviskletnieks27@gmail.com>
2022-10-03 12:49:36 +0300

    Inital Graph Setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,75 +1,45 @@
 @extends('wrapper')
-<?php
-
-$dataPoints = array(
-	array("x"=> 10, "y"=> 41),
-	array("x"=> 20, "y"=> 35, "indexLabel"=> "Lowest"),
-	array("x"=> 30, "y"=> 50),
-	array("x"=> 40, "y"=> 45),
-	array("x"=> 50, "y"=> 52),
-	array("x"=> 60, "y"=> 68),
-	array("x"=> 70, "y"=> 38),
-	array("x"=> 80, "y"=> 71, "indexLabel"=> "Highest"),
-	array("x"=> 90, "y"=> 52),
-	array("x"=> 100, "y"=> 60),
-	array("x"=> 110, "y"=> 36),
-	array("x"=> 120, "y"=> 49),
-	array("x"=> 130, "y"=> 41)
-);
-
-?>
 @section('content')
-<div class="container ">
-    <div class="dropdown show ">
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Locations
-        </a>
-
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Sensors
-          </a>
-
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-    </div>
-
-</div>
-
-<div class="container py-5">
-    <div id="chartContainer" style="height: 370px; width: 100%;">
-</div>
 <script>
-    window.onload = function () {
+window.onload = function() {
+
+var dataPoints = [];
 
 var chart = new CanvasJS.Chart("chartContainer", {
-    animationEnabled: true,
-    exportEnabled: true,
-    theme: "light1", // "light1", "light2", "dark1", "dark2"
-    title:{
-        text: "Change according to chosen sensor in location"
-    },
-    axisY:{
-        includeZero: true
-    },
-    data: [{
-        type: "column", //change type to bar, line, area, pie, etc
-        //indexLabel: "{y}", //Shows y value on all Data Points
-        indexLabelFontColor: "#5A5757",
-        indexLabelPlacement: "outside",
-        dataPoints:<?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK);?>
-    }]
+	animationEnabled: true,
+	theme: "light2",
+	zoomEnabled: true,
+	title: {
+		text: "Bitcoin Price - 2017"
+	},
+	axisY: {
+		title: "Price in USD",
+		titleFontSize: 24,
+		prefix: "$"
+	},
+	data: [{
+		type: "line",
+		yValueFormatString: "$#,##0.00",
+		dataPoints: dataPoints
+	}]
 });
-chart.render();
+
+function addData(data) {
+	var dps = data.price_usd;
+	for (var i = 0; i < dps.length; i++) {
+		dataPoints.push({
+			x: new Date(dps[i][0]),
+			y: dps[i][1]
+		});
+	}
+	chart.render();
 }
 
+$.getJSON("https://canvasjs.com/data/gallery/php/bitcoin-price.json", addData);
+
+}
 </script>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+
 @endsection
