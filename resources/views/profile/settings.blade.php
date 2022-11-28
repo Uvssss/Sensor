63b4d788ddccfb53fb87e3c5b0f9d147bfd012c1
Uvssss <uviskletnieks27@gmail.com>
2022-11-28 15:31:35 +0200

    Made /profile text visble

--- resources/views/profile/settings.blade.php
+++ resources/views/profile/settings.blade.php
@@ -1,7 +1,8 @@
 @extends('wrapper')
 @section('content')
 
-
-<h4>Make password reset, logout,activity,change pass</h4>
+<div class="container pt-7 pt-md-8">
+    <h4>Make password reset, logout,activity,change pass</h4>
+</div>
 
 @endsection
