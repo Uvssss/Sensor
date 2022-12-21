36e0331bbfef82d0e122a172eb2c338eea94ea00
Uvssss <uviskletnieks27@gmail.com>
2022-12-21 08:37:19 +0200

    About page- added content

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -1,9 +1,18 @@
 @extends('wrapper')
 @section('content')
 
-<div class="container-fluid  pt-7 pt-md-6">
-
-
+<div class="container-fluid pt-7 pt-md-6">
+    <div class="container pt-9 center text-center">
+        <h2>About Us</h2>
+        <hr style="background-color: black" size="5">
+        <h5>
+            We use your inserted sensor humidity and temperature data and show it as a graph.
+        </h5>
+    </div>
+    <div class="container  text-center">
+        <h4>Who we are?</h4>
+        <p><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, voluptatibus!</b></p>
+    </div>
 </div>
 
 
